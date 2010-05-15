d9e92a0249405c8ed8aad4524f98b511428ef6be
Taylan Pince <taylanpince@gmail.com>
2010-05-15 15:38:45 -0400

    Added basic styling

--- feedme/recommender/forms.py
+++ feedme/recommender/forms.py
@@ -6,12 +6,12 @@ class RecommendForm(forms.Form):
     """
     A form for getting a single domain for a recommendation
     """
-    url = forms.URLField(label=_("URL"), verify_exists=True)
+    url = forms.URLField(label=_("URL"), verify_exists=True, initial="huffingtonpost.com")
 
 
 class GoogleLoginForm(forms.Form):
     """
     A form for getting the user's Google credentials
     """
-    username = forms.CharField(label=_("Email"))
-    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    username = forms.CharField(label=_("Email"), initial="email")
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput, initial="password")
